12d3ed0b523808a1b162f73765ded5f3fa425fae
cuwoom <tolovefeels@naver.com>
2022-07-18 19:00:28 +0900

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,10 +1,6 @@
 const config = require(`../../.ghost.json`).production;
 module.exports = {
-    siteUrl:
-        process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
-            : `https://ghostblog12.herokuapp.com`, // Site domain. Do not include a trailing slash!
-
+    siteUrl: `https://ghostblog12.herokuapp.com`, // Site domain. Do not include a trailing slash!
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
